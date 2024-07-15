6710b8ddbb248348759765cc1316715c61ade017
mihili <mix.pavlovic@gmail.com>
2024-07-16 01:04:39 +0200

    Added section header table

--- inc/Elf32.hpp
+++ inc/Elf32.hpp
@@ -10,55 +10,85 @@ typedef uint32_t Elf32_Word;
 typedef int32_t  Elf32_SWord;
 
 
-#define EI_NIDENT       16
-#define EI_MAG0         0 /* File identification byte 0 index */
-#define EI_MAG1         1 /* File identification byte 1 index */
-#define EI_MAG2         2 /* File identification byte 2 index */
-#define EI_MAG3         3 /* File identification byte 3 index */
-#define EI_CLASS        4 /* File class byte index */
-#define EI_DATA         5 /* Data encoding byte index */
-#define EI_VERSION      6 /* File version byte index */
-#define EI_OSABI        7 /* OS ABI identification */
-#define EI_ABIVERSION   8 /* ABI version */
-#define EI_PAD          9 /* Byte index of padding bytes */
+// ****************************************************************************
+// *                                ELF HEADER                                *
+// ****************************************************************************
 
-// Struktura koja predstavlja header same ELF datoteke:
-struct Elf32_Ehrd { 
-    unsigned char e_ident[EI_NIDENT] {0x7f, 'E', 'L', 'F'};
-    Elf32_Half e_type;
-    Elf32_Half e_machine;
-    Elf32_Word e_version;
-    Elf32_Addr e_entry;
-    Elf32_Off e_phoff;
-    Elf32_Off e_shoff;
-    Elf32_Word e_flags;
-    Elf32_Half e_ehsize;
-    Elf32_Half e_phentsize;
-    Elf32_Half e_phnum;
-    Elf32_Half e_shentsize;
-    Elf32_Half e_shnum;
-    Elf32_Half e_shstrndx;
+// Makros used e_ident array in Elf32_Ehdr (ELF header)
+#define EI_NIDENT   8 /* ELF identification size */
+#define EI_MAG0     0 /* File identification byte 0 index */
+#define EI_MAG1     1 /* File identification byte 1 index */
+#define EI_MAG2     2 /* File identification byte 2 index */
+#define EI_MAG3     3 /* File identification byte 3 index */
+#define EI_CLASS    4 /* File class byte index */
+#define EI_DATA     5 /* Data encoding byte index */
+#define EI_VERSION  6 /* File version byte index */
+#define EI_OSABI    7 /* OS ABI identification */
+#define EI_PAD      8 /* Byte index of padding bytes */
+
+#define ELFCLASS32  1 /* 32-bit objects */
+#define ELFCLASS64  2 /* 64-bit objects */
+
+#define ELFDATA2LSB 1 /* Little-endian */
+#define ELFDATA2MSB 2 /* Big-endian */
+
+#define EV_NONE     0 /* Invalid version */
+#define EV_CURRENT  1 /* Current version */
+
+
+// Makros used e_type in Elf32_Ehdr (ELF header)
+#define ET_NONE 0 /* No file type */
+#define ET_REL  1 /* Relocatable file */
+#define ET_EXEC 2 /* Executable file */
+#define ET_DYN  3 /* Shared object file */
+
+
+// Struct that represents the ELF header
+struct Elf32_Ehdr {
+    unsigned char e_ident[EI_NIDENT] {0x7f, 'E', 'L', 'F', ELFCLASS32, ELFDATA2LSB, EV_CURRENT, 0};
+    Elf32_Half    e_type;       // File type
+    Elf32_Addr    e_entry;      // Entry point address
+    Elf32_Off     e_phoff;      // Program header table file offset
+    Elf32_Off     e_shoff;      // Section header table file offset
+    Elf32_Half    e_ehsize;     // ELF header size in bytes
+    Elf32_Half    e_phentsize;  // Program header table entry size
+    Elf32_Half    e_phnum;      // Program header table entry count
+    Elf32_Half    e_shentsize;  // Section header table entry size
+    Elf32_Half    e_shnum;      // Section header table entry count
+    Elf32_Half    e_shstrndx;   // Section header string table index entry
 };
 
 
-// Struktura koja predstavlja sekciju tabele simbola
-struct Elf32_Shrd {
-    uint32_t sh_offset;   // Pomeraj od pocetka ELF datoteke do lokacije gde pocinje sym table
-    uint32_t sh_size;     // Velicina sekcije symbol table izrazeno u broju bajtova
-    uint32_t sh_link;     // Indeks zaglavlja sekcije pridruzene tabele stringova
-    uint32_t sh_info;     // Vrednost za jedan veca od indeksa poslednjeg lokalnog simbola u sym table
-    uint32_t sh_entsize;  // Velicina jednog ulaza unutar sym table
-};
+// ****************************************************************************
+// *                           SECTION HEADER TABLE                           *
+// ****************************************************************************
 
 
-// Struktura za ulaz u tabelu simbola:
-struct Elf32_Sym {
-    Elf64_Word    st_name;
-    unsigned char st_info;
-    Elf64_Half    st_other;
-    Elf64_Addr    st_value;
-    Elf64_XWord   st_size;
-};
+#define SHN_UNDEF     0      /* Undefined section */
+#define SHN_LORESERVE 0xff00 /* Start of reserved indices */
+#define SHN_LOPROC    0xff00 /* Start of processor-specific */
+#define SHN_HIPROC    0xff1f /* End of processor-specific */
+#define SHN_LOOS      0xff20 /* Start of OS-specific */
+#define SHN_HIOS      0xff3f /* End of OS-specific */
+#define SHN_ABS       0xfff1 /* Associated symbol is absolute */
+#define SHN_COMMON    0xfff2 /* Associated symbol is common */
+#define SHN_XINDEX    0xffff /* Index is in extra table. */
+#define SHN_HIRESERVE 0xffff /* End of reserved indices */
+
+
+// Struct that represents the symbol table entry
+typedef struct {
+    Elf32_Word sh_name;   // Section name (string table index)
+    Elf32_Word sh_type;   // Section type, not used
+    Elf32_Word sh_flags;  // Section attributes, not used
+    Elf32_Addr sh_addr;   // Virtual address in memory
+    Elf32_Off  sh_offset; // Offset from start of file in bytes 
+    Elf32_Word sh_size;
+    Elf32_Word sh_link;
+    Elf32_Word sh_info;
+    Elf32_Word sh_addralign;
+    Elf32_Word sh_entsize;
+} Elf32_Shdr;
 
 
 #endif
